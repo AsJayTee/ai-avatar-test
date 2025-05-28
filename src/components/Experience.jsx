d45abf025f37caba7498bf2227ed63f281bd1db5
AsJayTee <jinthauofficial@gmail.com>
2025-05-28 14:06:56 +0800

    Added all models

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
-import { Avatar } from "./Avatar";
+import { Avatar } from "./Avatar-JT";
 import { useThree } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
